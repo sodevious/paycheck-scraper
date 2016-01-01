d996c39bfe68e811287a18f290931ebfb9d7a213
Nicole Dominguez <sodevious.net@gmail.com>
2016-01-01 16:40:12 -0500

    get net pay, total and deductions working

--- scraper.py
+++ scraper.py
@@ -7,16 +7,30 @@ record = BeautifulSoup(open("src/Paycheck-2015-12-24.html"), "html.parser")
 # Get paystub dates
 date = record.find(id="paystub_form_tbl").td.get_text(strip=True).strip("Pay stub for period:").lstrip()
 
+# Get the summary section
+summary_table = record.find(id="paystub_summary_tbl").find_all('div')
+
+# Create a list
+summaries = []
+
+# Clean data
+for item in summary_table:
+	item = item.get_text("", strip=True).encode('utf-8')
+	summaries.append(item)
+
+# Create items from cleaned array
+total = summaries[0]
+deductions = summaries[2]
+taxes = summaries[4]
+
 # Sample
-salary = "Salary"
-net = "Net Pay"
-reimbursement = "Reimbursement"
-deductions = "Deductions"
-insurance = "Health Insurance"
-taxes= "Taxes Paid"
+hours = "Hours"
+rate = "Rate"
+net = "net"
 
 # Write to CSV
-f = csv.writer(open("data.csv", "w"))
-f.writerow([date, salary, net, reimbursement, deductions, insurance, taxes])
+f = csv.writer(open("src/paycheck_data.csv", "w"))
+f.writerow(["Date", "Total Pay", "Net Pay", "Hours", "Rate", "Deductions"]) # Write column headers as the first line
+f.writerow([date, total, net, hours, rate, deductions])
 
-print date, salary, net, reimbursement, deductions, insurance, taxes
+print date, total, deductions, taxes, net, hours, rate
--- test.py
+++ test.py
@@ -1,4 +1,50 @@
+import glob
+import os
+import csv
+from bs4 import BeautifulSoup
 
+input_dir = "/Users/nicoledominguez/sites/paycheck-scraper/src/html/"
+output_dir = "/Users/nicoledominguez/sites/paycheck-scraper/src/"
+
+
+# Open the HTML file
+# record = BeautifulSoup(open("src/html/Paycheck-2015-12-24.html"), "html.parser")
+
+f = csv.writer(open("src/paycheck_data.csv", "w"))
+
+
+for file_name in glob.glob(input_dir+ "*.html"):
+    with open(file_name) as fp:
+        record = BeautifulSoup(fp, "html.parser")
+	
+	date = record.find(id="paystub_form_tbl").td.get_text(strip=True).strip("Pay stub for period:").lstrip()
+
+	# f.writerow(["Date", "Total Pay", "Net Pay", "Hours", "Rate", "Deductions"]) # Write column headers as the first line
+	
+	print file_name
+
+	f.writerow([date])
+
+   
+
+
+summary = record.find(id="paystub_summary_tbl").find_all('div')
+
+rows = []
+
+for row in summary:
+	row = row.get_text(",", strip=True).encode('utf-8')
+	rows.append(row)
+
+total = rows[:1]
+#print ",".join(str(x) for x in total)
+
+
+
+
+
+# print rows
+# print ', '.join([str(ro) for ro in rows])
 
 
 
