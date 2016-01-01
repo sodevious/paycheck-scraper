132054eb6148ef6abda88898c8dbefd2a1f2a754
Nicole Dominguez <sodevious.net@gmail.com>
2016-01-01 17:52:27 -0500

    Add net

--- scraper_hourly.py
+++ scraper_hourly.py
@@ -21,20 +21,25 @@ for file_name in glob.glob(input_dir+ "*.html"):
 	# Get the relevant sections
 	summary_table = record.find(id="paystub_summary_tbl").find_all('div')
 	pay_table = record.find(id="paystub_pay_tbl").find_all('div')
+	net_table = record.find(id="paystub_net_tbl").find_all('td')
 
 	# Create lists
 	summaries = []
 	pay_rows = []
+	net_rows = []
 
 	# Clean data
 	for item in summary_table:
 		item = item.get_text("", strip=True).encode('utf-8')
 		summaries.append(item)
 
-	# And more data
 	for item in pay_table:
 		item = item.get_text(",", strip=True).encode('utf-8')
 		pay_rows.append(item)
+
+	for item in net_table:
+		item = item.get_text(",", strip=True).encode('utf-8')
+		net_rows.append(item)
 
 	# Create items from cleaned arrays
 	total = summaries[0]
@@ -42,14 +47,13 @@ for file_name in glob.glob(input_dir+ "*.html"):
 	taxes = summaries[4]	
 	hours = pay_rows[4]
 	rate = pay_rows[5]
-
-	# Sample
-	net = "net"
+	net = net_rows[1]
 
 	f.writerow([date, total, net, hours, rate, deductions])
 
 	print date, total, deductions, taxes, net, hours, rate
 
+# Add timestamp
 generated = time.strftime("%I:%M:%S, %d/%m/%Y")
 
 f.writerow(["Generated:", generated, " ", " ", " ", " "])
