3514030a8978b78d6abbf4ec1b720127e99f1363
Nicole Dominguez <sodevious.net@gmail.com>
2016-01-01 12:46:09 -0500

    write extracted data to csv, set up sample data

--- scraper.py
+++ scraper.py
@@ -1,8 +1,22 @@
 from bs4 import BeautifulSoup
 import csv
 
+# Open the HTML file
 record = BeautifulSoup(open("src/Paycheck-2015-12-24.html"), "html.parser")
 
-date = record.find(id="paystub_form_tbl")
+# Get paystub dates
+date = record.find(id="paystub_form_tbl").td.get_text(strip=True).strip("Pay stub for period:").lstrip()
 
-print "Date: " + date.td.get_text(strip=True).strip("Pay stub for period:").lstrip()
+# Sample
+salary = "Salary"
+net = "Net Pay"
+reimbursement = "Reimbursement"
+deductions = "Deductions"
+insurance = "Health Insurance"
+taxes= "Taxes Paid"
+
+# Write to CSV
+f = csv.writer(open("data.csv", "w"))
+f.writerow([date, salary, net, reimbursement, deductions, insurance, taxes])
+
+print date, salary, net, reimbursement, deductions, insurance, taxes
