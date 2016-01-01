8252929c3248129660c162910b1425e17a7d8225
Nicole Dominguez <sodevious.net@gmail.com>
2016-01-01 12:34:50 -0500

    scrape date

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import csv
 
-# record = BeautifulSoup (open("test.html"))
-record = BeautifulSoup (open("src/Paycheck-2015-12-24.html"))
+record = BeautifulSoup(open("src/Paycheck-2015-12-24.html"), "html.parser")
 
-print record
+date = record.find(id="paystub_form_tbl")
+
+print "Date: " + date.td.get_text(strip=True).strip("Pay stub for period:").lstrip()
---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+
+
+#net = record.find(class="paycheck")
+
+#print net.get_text(",", strip=True)
+
+#for td in net_td:
+	#print td.get_text(",", strip=True)
+
+# net = record.find(id="paystub_net_tbl")
+#print(net, net_td)
+# print(record.prettify())
